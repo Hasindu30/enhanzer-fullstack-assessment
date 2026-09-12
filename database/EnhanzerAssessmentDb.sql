6e916cdbfc57475b26f9e99f04cff90af2f48084
Hasindu Chandeepa <chandeepahasindu@gmail.com>
2026-09-12 22:12:51 +0530

    add setup instructions database script and screenshots

---
+++ database/EnhanzerAssessmentDb.sql
@@ -0,0 +1,27 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Location_Details] (
+    [Id] int NOT NULL IDENTITY,
+    [LocationCode] nvarchar(50) NOT NULL,
+    [LocationName] nvarchar(200) NOT NULL,
+    CONSTRAINT [PK_Location_Details] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260912054717_CreateLocationDetailsTable', N'8.0.0');
+GO
+
+COMMIT;
+GO
